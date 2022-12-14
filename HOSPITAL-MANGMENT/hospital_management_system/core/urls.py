531af56e9eb7a26dee6c7a17aba55fde3875c412
enas <enasabo2811@gmail.com>
2022-12-14 21:23:53 +0200

    name for views

--- HOSPITAL-MANGMENT/hospital_management_system/core/urls.py
+++ HOSPITAL-MANGMENT/hospital_management_system/core/urls.py
@@ -3,10 +3,8 @@ from .views import *
 urlpatterns=[
     
     path('',index) ,
-    path('patient_signup/',patient_signup),
-    path('login_Doctor/',login_Doctor),
-    path('signup_Doctor/',signup_Doctor),
-    path('login_patient/',login_patient),
-
-
+    path('patient_signup/',patient_signup,name="patient_signup"),
+    path('login_Doctor/',login_Doctor,name="login_Doctor"),
+    path('signup_Doctor/',signup_Doctor,name="signup_Doctor"),
+    path('login_patient/',login_patient,name="login_patient"),
     ]
