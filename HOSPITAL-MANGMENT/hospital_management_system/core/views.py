fe4dc97fbbc87e41d04a08fba94ae9513b3ad5f9
AnfalNb <118118469+AnfalNb@users.noreply.github.com>
2022-12-14 16:20:55 +0200

    change template loction

--- HOSPITAL-MANGMENT/hospital_management_system/core/views.py
+++ HOSPITAL-MANGMENT/hospital_management_system/core/views.py
@@ -1,9 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+ 
 # Create your views here.
 
 def index(requst):
     return HttpResponse("<h1> hospital mangment system website")
 
 
+
+def patient_signup(requst):
+    return render(requst,'patient_signup.html')
