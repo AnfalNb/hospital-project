c6a19724c21e8d6ddf670f6794070a943d8df0c7
AnfalNb <118118469+AnfalNb@users.noreply.github.com>
2022-12-15 08:47:27 +0200

    FIX URLSS

--- HOSPITAL-MANGMENT/hospital_management_system/core/urls.py
+++ HOSPITAL-MANGMENT/hospital_management_system/core/urls.py
@@ -5,5 +5,7 @@ urlpatterns=[
     path('patient_signup/',patient_signup),
     path('login_Doctor/',login_Doctor),
     path('signup_Doctor/',signup_Doctor),
-
-    ]
+    path('login_patient/',login_patient,name="login_patient"),
+    path('AdminLogin/',AdminLogin,name="AdminLogin"),
+    path('AskDoctor/',AskDoctor,name="AskDoctor"),
+]
